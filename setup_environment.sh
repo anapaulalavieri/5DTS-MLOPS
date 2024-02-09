360d394bd43e1bd36fdd69dfb646e815ef72614d
Ana Lavieri <anapaulalavieri@gmail.com>
2024-02-09 16:40:48 -0300

    add

---
+++ setup_environment.sh
@@ -0,0 +1,8 @@
+echo "Creating docker images for all containers"
+docker build -t api_part2 -f Parte_2/dockerbuilds/Dockerfile Parte_2/api/
+
+echo "Creating network"
+docker network create plat_network
+
+echo "Deploying containers for predictions"
+docker run -d --restart always --network plat_network --name api_part2 api_part2
