2ac1ab703690d3cc8ca76732fb58b72a8c7b9228
Ana Lavieri <anapaulalavieri@gmail.com>
2024-02-09 22:44:42 -0300

    add

--- Parte_3/update_config.sh
+++ Parte_3/update_config.sh
@@ -1,13 +1,13 @@
 echo '{                                                     ' >  $(pwd)/Parte_3/api/config/microservices.json
 echo '  "models": {                                         ' >> $(pwd)/Parte_3/api/config/microservices.json
-echo '    "default_propensity": {                                     ' >> $(pwd)/Parte_3/api/config/microservices.json
+echo '    "parte1_api": {                                     ' >> $(pwd)/Parte_3/api/config/microservices.json
 echo '      "version": "V01",                               ' >> $(pwd)/Parte_3/api/config/microservices.json
-echo '      "url": "http://'$(sudo docker inspect defaultpropensityapi | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
+echo '      "url": "http://'$(sudo docker inspect parte1_api | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['plat_network']['IPAddress'])")':8080/predict"          ' >> $(pwd)/Parte_3/api/config/microservices.json
 echo '    },                                                ' >> $(pwd)/Parte_3/api/config/microservices.json
-echo '    "customer_clustering": {                                     ' >> $(pwd)/Parte_3/api/config/microservices.json
+echo '    "parte2_api": {                                     ' >> $(pwd)/Parte_3/api/config/microservices.json
 echo '      "version": "V01",                               ' >> $(pwd)/Parte_3/api/config/microservices.json
-echo '      "url": "http://'$(sudo docker inspect customerclusteringapi | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
+echo '      "url": "http://'$(sudo docker inspect parte2_api | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['plat_network']['IPAddress'])")':8080/predict"          ' >> $(pwd)/Parte_3/api/config/microservices.json
 echo '    }                                                 ' >> $(pwd)/Parte_3/api/config/microservices.json
 echo '  }                                                   ' >> $(pwd)/Parte_3/api/config/microservices.json
