5108aa6e52c4a551fd6f9097a7e9fa33a82010a8
Ana Lavieri <anapaulalavieri@gmail.com>
2024-02-10 10:30:13 -0300

    add

--- Parte_2/api/api_parte2.py
+++ Parte_2/api/api_parte2.py
@@ -100,7 +100,7 @@ def predict():
 @app.route("/", methods=['GET', 'POST'])
 def call_home(request = request):
     print(request.values)
-    return "Server is ready to be used!\n"
+    return "Server is ready to be used!!!\n"
 
 if __name__ == '__main__':
     init()
