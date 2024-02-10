13d424d6ed60917345594662cea5d4df31bac4f3
Ana Lavieri <anapaulalavieri@gmail.com>
2024-02-10 20:19:15 -0300

    add

--- reset_environment.sh
+++ reset_environment.sh
@@ -2,11 +2,13 @@
 docker stop parte1_api 
 docker stop parte2_api 
 docker stop modelmanager
+docker stop frontendstreamlit
 
 # Removendo os containers
 docker rm parte1_api
 docker rm parte2_api
 docker rm modelmanager
+docker rm frontendstreamlit
 
 # Removendo a network
 docker network rm plat_network
