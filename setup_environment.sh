20979639064b35108b087dfad0ef23652763ffe4
Ana Lavieri <anapaulalavieri@gmail.com>
2024-02-10 13:25:57 -0300

    add

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,6 +1,9 @@
 echo "Creating docker images for all containers"
+echo "Building Parte_1 --------------------------------------------------------------------"
 docker build -t parte1_api -f Parte_1/dockerbuilds/Dockerfile Parte_1/api/
+echo "Building Parte_2 --------------------------------------------------------------------"
 docker build -t parte2_api -f Parte_2/dockerbuilds/Dockerfile Parte_2/api/
+echo "Building Parte_3 --------------------------------------------------------------------"
 docker build -t modelmanager -f Parte_3/dockerbuilds/Dockerfile Parte_3/api/
 
 echo "Creating network"
@@ -15,10 +18,4 @@ bash ./Parte_3/update_config.sh
 echo "Config model manager"
 docker run -d --restart always --network plat_network -v $(pwd)/Parte_3/api/config:/myServer/config -v $(pwd)/Parte_3/docker/log:/myServer/log --name modelmanager modelmanager
 
-echo "Waiting for containers to start..."
-sleep 10
-
-docker images
-docker network ls
-docker ps
 docker network inspect plat_network
