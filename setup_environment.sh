f2b3049f18af94c92acdd7a30dc476a41ad585a4
Ana Lavieri <anapaulalavieri@gmail.com>
2024-02-09 21:58:28 -0300

    add

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,8 +1,15 @@
 echo "Creating docker images for all containers"
 docker build -t parte2_api -f Parte_2/dockerbuilds/Dockerfile Parte_2/api/
+docker build -t modelmanager -f part_3/dockerbuilds/Dockerfile part_3/api/
 
 echo "Creating network"
 docker network create plat_network
 
 echo "Deploying containers for predictions"
 docker run -d --restart always --network plat_network --name parte2_api parte2_api
+
+bash ./Parte_3/update_config.sh
+
+echo "Config model manager"
+docker run -d --restart always --network plat_network -v $(pwd)/Parte_3/api/config:/myServer/config -v $(pwd)/Parte_3/docker/log:/myServer/log --name modelmanager modelmanager
+
